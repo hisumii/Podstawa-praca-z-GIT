0c50a31372d1f8376ef18ae68bf7198fa165f86e
Martyna <dsw56073@student.dsw.edu.pl>
2025-05-07 02:12:31 +0200

    add: logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,8 @@ if [[ "$1" == "--date" ]]; then
     date
 fi
 
+if [[ "$1" == "--logs" ]]; then
+    for i in {1..100}; do
+        echo -e "Nazwa pliku: log$i.txt\nNazwa skryptu: $0\nData: $(date "+%Y-%m-%d %H:%M:%S")" > log$i.txt
+    done
+fi
