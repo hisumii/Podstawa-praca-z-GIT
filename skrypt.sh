bfd874d0ec2f97a3292464316dd301cc098d25df
Martyna <dsw56073@student.dsw.edu.pl>
2025-05-07 02:20:58 +0200

    add: count in logs

--- skrypt.sh
+++ skrypt.sh
@@ -4,7 +4,9 @@ if [[ "$1" == "--date" ]]; then
 fi
 
 if [[ "$1" == "--logs" ]]; then
-    for i in {1..100}; do
+    count=${2:-100}
+    for ((i=1; i<=count; i++)); do
         echo -e "Nazwa pliku: log$i.txt\nNazwa skryptu: $0\nData: $(date "+%Y-%m-%d %H:%M:%S")" > log$i.txt
     done
 fi
+
